99e1dd355d908770315dad04d1a39c980c0f7e4a
Yotam Aviv <yotam@aviv.net>
2025-02-24 15:37:31 -0500

    functions sync'd with Nerdster

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,24 @@
-// TODO:
-// Pressing:
-// - Fix Yotam data corruption, would be nice to get support from export with ids (tokens)
-// - Prototype performance enhancements on Oneofus (would be nice to have identical index.js functions file)
-// - integration tests
-//   - Implement, test: revokedAt
-// Can be later:
-// - Clean this up
-// - Organize the file, use Javascript helpers and constants
-// - try to unify Nerdster and Oneofus. Any reason they can't be identical?
-//   - different verbs, but I can just include all verbs, no worries
-// - JavaScript unit testing
-// - Export clouddistinct to the HTTP interface (at least for debugging, demonstrating..) 
-// - Rename "id" to "token"
-// - Test boundary condition of empty// - 
-// 
-// I often forget and then see it in the logs.. (to run in the functions directory)
-// - "npm install"
-// - "npm install --save firebase-functions@latest"
-// - "npm audit fix"
-// 
+
+/// 
+/// - DONE: Try to unify this file for both Nerdster and Oneofus. Any reason they can't be identical?
+///   - (different verbs, but I can just include all verbs, no worries)
+/// - DONE: JavaScript unit testing
+/// - DONE: Export clouddistinct to the HTTP interface (at least for debugging, demonstrating..) 
+/// - Rename "id" to "token"
+/// - Test boundary condition of empty. This is on the Fetcher/Nerdster side, not really here.
+/// 
+/// I often forget and then see it in the logs.. (to run in the functions directory)
+/// - "npm install"
+/// - "npm install --save firebase-functions@latest"
+/// - "npm audit fix"
+/// 
+/// TEST: Would be nice to see that these all produce output we expect:
+/// http://127.0.0.1:5001/nerdster/us-central1/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b&bIncludeId=true&bOrderStatements=true&bValidate=true&revokeAt=254267baf5859ba52100f42c3df6aebc4be6dc56
+/// http://127.0.0.1:5001/nerdster/us-central1/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b&bIncludeId=true&bOrderStatements=true&bValidate=true&revokeAt=sincealways
+/// http://127.0.0.1:5001/nerdster/us-central1/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b&bIncludeId=true&bOrderStatements=true&bDistinct=true
+/// http://127.0.0.1:5001/nerdster/us-central1/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b&bIncludeId=true&bOrderStatements=true
+/// http://127.0.0.1:5001/nerdster/us-central1/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b&bIncludeId=true&bOrderStatements=true&bClearClear=true
+/// http://127.0.0.1:5001/nerdster/us-central1/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b&bIncludeId=true&bOrderStatements=true&bDistinct=true&bClearClear=true&omit=[%22I%22,%22statement%22]
 
 const { logger } = require("firebase-functions");
 const { onRequest } = require("firebase-functions/v2/https");
@@ -30,7 +30,6 @@ const fetch = require("node-fetch");
 const cheerio = require('cheerio'); // For HTML parsing
 
 admin.initializeApp();
-
 
 // This works and is used to develop fetchtitle below.
 // Take the url parameter passed to this HTTP endpoint and insert it into
@@ -72,22 +71,254 @@ exports.fetchtitle = onDocumentCreated("/urls/{documentId}", async (event) => {
 // Statement'ish needs here in JavaScript:
 // - get subject of verb for  distinct based on subjects.
 
-// Demo / debugging needs
-// Export the cloud functions, include id/token
+// ----------- copy/pasted from <nerdster>/js ---------------------------------------------------//
 
-// Performance needs / desires
-// - Distinct
-//   Doesn't have to be complete and correct to be helpful
-// - revokedAt 
-//   required as applying distinct may clear the revokedAt token.
-// - Filters, like say, fetch?verbs={censor:all, rate:month}.. complicated.. not necessarily helpful '
-//   anyway considering where "dis" is, how I either should or shouldn't an entire subject..
-// 
+var key2order = {
+  "statement": 0,
+  "time": 1,
+  "I": 2,
+  "trust": 3,
+  "block": 4,
+  "replace": 5,
+  "delegate": 6,
+  "clear": 7,
+  "rate": 8,
+  "censor": 9,
+  "relate": 10,
+  "dontRelate": 11,
+  "equate": 12,
+  "dontEquate": 13,
+  "follow": 14,
+  "with": 16,
+  "other": 17,
+  "moniker": 18,
+  "revokeAt": 19,
+  "domain": 20,
+  "tags": 21,
+  "recommend": 22,
+  "dismiss": 23,
+  "stars": 24,
+  "comment": 25,
+  "contentType": 26,
+  "previous": 27,
+  "signature": 28
+};
+
+async function computeSHA1(str) {
+  const buffer = new TextEncoder("utf-8").encode(str);
+  const hash = await crypto.subtle.digest("SHA-1", buffer);
+  return Array.from(new Uint8Array(hash))
+    .map(x => x.toString(16).padStart(2, '0'))
+    .join('');
+}
+
+function compareKeys(key1, key2) {
+  // console.log(`compareKeys(${key1}, ${key2})`);
+  // Keys we know have an order; others are ordered alphabetically below keys we know except signature.
+  // TODO: Is that correct about 'signature' below unknown keys?
+  const key1i = key2order[key1];
+  const key2i = key2order[key2];
+  var out;
+  if (key1i != null && key2i != null) {
+    out = key1i - key2i;
+  } else if (key1i == null && key2i == null) {
+    out = key1 < key2 ? -1 : 1;
+  } else if (key1i != null) {
+    out = -1;
+  } else {
+    out = 1;
+  }
+  // console.log(`compareKeys(${key1}, ${key2})=${out}`);
+  return out;
+}
+
+// BUG: Need to special case on signature which always goes last.
+// (not pressing as none of my statements have unknown top level keys)
+function order(thing) {
+  if (typeof thing === 'string') {
+    return thing;
+  } else if (typeof thing === 'boolean') {
+    return thing;
+  } else if (typeof thing === 'number') {
+    return thing;
+  } else if (Array.isArray(thing)) {
+    return thing.map((x) => order(x));
+  } else {
+    return Object.keys(thing)
+      .sort((a, b) => compareKeys(a, b))
+      .reduce((obj, key) => {
+        obj[key] = order(thing[key]);
+        return obj;
+      }, {});
+  }
+}
+
+async function keyToken(input) {
+  if (typeof input === 'string') {
+    return input;
+  } else {
+    const ordered = order(input);
+    var ppJson = JSON.stringify(ordered, null, 2);
+    var token = await computeSHA1(ppJson);
+    return token;
+  }
+}
+
+// -----------  --------------------------------------------------------//
+
+const verbs = [
+  'trust',
+  'delegate',
+  'clear',
+  'rate',
+  'follow',
+  'censor',
+  'relate',
+  'dontRelate',
+  'equate',
+  'dontEquate',
+  'replace',
+  'block',
+];
+
+function getVerbSubject(j) {
+  for (var verb of verbs) {
+    if (j[verb] != null) {
+      return [verb, j[verb]];
+    }
+  }
+  return null;
+}
+
+// -----------  --------------------------------------------------------//
+
+/// DEFER: Cloud distinct to regard "other" subject.
+/// All the pieces are there, and it shouldn't be hard. That said, relate / equate are rarely used.
+
+/// CONSIDER: Do we really need "I" or "lastToken"?
+/// - revokedAt means we're revoked, and so we shouldn't be writing new statements anyway. But 
+///   maybe we do.
+/// - if we send "clear" statements, then we'll always send the last statement (even if it's clear, 
+///   even if we make distinct)
+/// - If we're omitting "I", we can still include it on the top statemement.
+/// All this feels kludgey, and so I'll leave things as they are.
+
+// bClearClear only applicable with bDistinct
+async function fetchh(token, params = {}, omit = {}) {
+  const revokeAt = params.revokeAt;
+  const bValidate = params.bValidate != null;
+  const bDistinct = params.bDistinct != null;
+  const bOrderStatements = params.bOrderStatements != null;
+  const bClearClear = params.bClearClear != null;
+  const bIncludeId = params.bIncludeId != null;
+
+  if (!token) throw 'Missing token';
+  if (bClearClear && !bDistinct) throw 'bClearClear only applicable with bDistinct';
+
+  const db = admin.firestore();
+  const collectionRef = db.collection(token).doc('statements').collection('statements');
+
+  var revokedAtTime;
+  if (revokeAt) {
+    const doc = collectionRef.doc(revokeAt);
+    const docSnap = await doc.get();
+    if (docSnap.data()) {
+      logger.log(`found revokedAt doc`);
+      revokedAtTime = docSnap.data().time;
+      logger.log(`revokedAtTime=${revokedAtTime}`);
+    } else {
+      logger.log(`didn't find revokedAt doc`);
+      // TODO: Boundary conditions testing.
+      return { "statements": [] };
+    }
+  }
+
+  var snapshot;
+  if (revokedAtTime) {
+    snapshot = await collectionRef.where('time', "<=", revokedAtTime).orderBy('time', 'desc').get();
+  } else {
+    snapshot = await collectionRef.orderBy('time', 'desc').get();
+  }
+
+  var statements;
+  if (bIncludeId) {
+    statements = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  } else {
+    statements = snapshot.docs.map(doc => doc.data());
+  }
+
+  // Do this early (first) before distinct and/or other calls below.
+  var iKey;
+  var lastToken;
+  if (statements.length > 0) {
+    iKey = statements[0].I;
+    lastToken = statements[0].id;
+  }
+
+  if (omit) {
+    for (var s of statements) {
+      for (const key of omit) {
+        delete s[key];
+      }
+    }
+  }
+
+  if (bValidate) {
+    // Validate notary chain, decending order
+    var first = true;
+    var previousToken;
+    var previousTime;
+    for (var d of statements) {
+      if (first) {
+        first = false; // no check
+      } else {
+        if (d.id != previousToken) {
+          var error = `Notarization violation: ${d.id} != ${previousToken}`;
+          logger.error(error);
+          throw error;
+        }
+
+        if (d.time >= previousTime) {
+          var error = `Not descending: ${d.time} >= ${previousTime}`;
+          logger.error(error);
+          throw error;
+        }
+      }
+      previousToken = d.previous;
+      previousTime = d.time;
+    }
+  }
+
+  if (bDistinct) {
+    statements = await makedistinct(statements, bClearClear);
+  }
+
+  // order statements
+  if (bOrderStatements) {
+    var orderedStatements = [];
+    for (const datum of statements) {
+      const orderedDatum = Object.keys(datum)
+        .sort((a, b) => ((key2order[a] ?? 40) - (key2order[b] ?? 40)))
+        // .sort((a, b) => compareKeys(a, b))
+        .reduce((obj, key) => {
+          obj[key] = datum[key];
+          return obj;
+        }, {});
+      orderedStatements.push(orderedDatum);
+    }
+    statements = orderedStatements;
+  }
+
+  return { "statements": statements, "I": iKey, "lastToken": lastToken };
+}
+
 
 // JSON export
 // from: Google AI: https://www.google.com/search?q=Firebase+function+HTTP+GET+export+collection&oq=Firebase+function+HTTP+GET+export+collection&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRhA0gEIOTYzMmowajSoAgCwAgE&sourceid=chrome&ie=UTF-8
-// - Emulator-Nerdster-Yotam: http://127.0.0.1:5001/nerdster/us-central1/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b
-// - Emulator-Oneofus-Yotam: http://127.0.0.1:5002/one-of-us-net/us-central1/export2?token=2c3142d16cac3c5aeb6d7d40a4ca6beb7bd92431
+// - Emulator-Nerdster-Yotam: 
+//   http://127.0.0.1:5001/nerdster/us-central1/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b
+// - Emulator-Oneofus-Yotam:
+//   http://127.0.0.1:5002/one-of-us-net/us-central1/export2?token=2c3142d16cac3c5aeb6d7d40a4ca6beb7bd92431&bIncludeId=true&bOrderStatements=true
 // - Prod-Nerdster-Yotam: https://us-central1-nerdster.cloudfunctions.net/export2?token=f4e45451dd663b6c9caf90276e366f57e573841b
 // - Prod-Oneofus-Yotam: http://us-central1-one-of-us-net.cloudfunctions.net/export2?token=2c3142d16cac3c5aeb6d7d40a4ca6beb7bd92431
 // 
@@ -98,71 +329,15 @@ exports.fetchtitle = onDocumentCreated("/urls/{documentId}", async (event) => {
 //   - https://console.firebase.google.com/project/nerdster/functions/list
 exports.export2 = onRequest(async (req, res) => {
   const token = req.query.token;
-  if (!token) return res.status(400).send('Missing token');
-
-  // I'm not commenting out the Oneofus verbs because I often run the emulator from the nerdster 
-  // directory. Sloppy, not correct..
-  const key2order = {
-    'statement': 0, 'time': 1, 'I': 2,
-    'clear': 7,
-    // Oneofus verbs
-    'trust': 3, 'block': 4, 'replace': 5, 'delegate': 6,
-    // Nerdster verbs
-    'rate': 8, 'censor': 9, 'relate': 10, 'dontRelate': 11, 'equate': 12, 'dontEquate': 13, 'follow': 14,
-    'with': 16,
-    // Oneofus with
-    'moniker': 18, 'revokeAt': 19, 'domain': 20,
-    // Nerdster with
-    'tags': 21, 'recommend': 22, 'dismiss': 23, 'stars': 24, 'comment': 25, 'contentType': 26, 'other': 17,
-    'previous': 27, 'signature': 28
-  };
-
-  // This works, but we're not recursing into the Maps or Lists, and so there's no need for it.
-  // DEFER: Port more from Jsonish to sort the keys for display
-  // function compareKeys(key1, key2) {
-  //   // Keys we know have an order.
-  //   // Keys we don't know are ordered alphabetically below keys we know except signature.
-  //   const key1i = key2order[key1];
-  //   const key2i = key2order[key2];
-  //   var out;
-  //   if (key1i != null && key2i != null) {
-  //     out = key1i - key2i;
-  //   } else if (key1i == null && key2i == null) {
-  //     out =  key1.compareTo(key2);
-  //   } else if (key1i != null) {
-  //     out =  -1;
-  //   } else {
-  //     out =  1;
-  //   }
-  //   logger.log(`${key1} ${key2} ${out}`);
-  //   return out;
-  // }
-
+  const omit = req.query.omit ? JSON.parse(req.query.omit) : null;
   try {
-    const db = admin.firestore();
-    const collectionRef = db.collection(token).doc('statements').collection('statements');
-    const snapshot = await collectionRef.orderBy('time', 'desc').get();
-    const data = snapshot.docs.map(doc => doc.data());
-
-    var data2 = [];
-    for (const datum of data) {
-      const orderedDatum = Object.keys(datum)
-        .sort((a, b) => ((key2order[a] ?? 40) - (key2order[b] ?? 40)))
-        // .sort((a, b) => compareKeys(a, b))
-        .reduce((obj, key) => {
-          obj[key] = datum[key];
-          return obj;
-        }, {});
-      data2.push(orderedDatum);
-    }
-
-    res.status(200).json(data2);
+    const statements = await fetchh(token, req.query, omit);
+    res.status(200).json(statements);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error exporting collection');
+    res.status(500).send(`Error: ${error}`);
   }
 });
-
 
 // HTTP POST for QR signin (not 'signIn' (in camelCase) - that breaks things).
 // The Nerdster should be listening for a new doc at collection /sessions/doc/<session>/
@@ -179,69 +354,8 @@ exports.signin = onRequest((req, res) => {
     });
 });
 
-async function computeSHA1(str) {
-  const buffer = new TextEncoder("utf-8").encode(str);
-  const hash = await crypto.subtle.digest("SHA-1", buffer);
-  return Array.from(new Uint8Array(hash))
-    .map(x => x.toString(16).padStart(2, '0'))
-    .join('');
-}
-
-function sortDictionaryByKey(dictionary) {
-  const sortedKeys = Object.keys(dictionary).sort();
-  const sortedDictionary = {};
-  // TODO: Generalize this and merge with the export stuff
-  if ("contentType" in dictionary) {
-    sortedDictionary["contentType"] = dictionary["contentType"];
-    // delete dictionary.contentType; // This delete makes it not be in sortedDictionary.
-  }
-  sortedKeys.forEach(key => {
-    if (key != "contentType") {
-      sortedDictionary[key] = dictionary[key];
-    }
-  });
-  return sortedDictionary;
-}
-
-async function keyToken(input) {
-  if (typeof input === 'string') {
-    return input;
-  } else {
-    const sortedDict = sortDictionaryByKey(input);
-    var ppJson = JSON.stringify(sortedDict, null, 2);
-    var token = await computeSHA1(ppJson);
-    return token;
-  }
-}
-
-
-const verbs = [
-  'rate',
-  'clear',
-  'follow',
-  'censor',
-  'relate',
-  'dontRelate',
-  'equate',
-  'dontEquate',
-
-  'trust',
-  'delegate',
-  'clear',
-  'replace',
-  'block',
-];
-
-function getVerbSubject(j) {
-  for (var verb of verbs) {
-    if (j[verb] != null) {
-      return [verb, j[verb]];
-    }
-  }
-  return null;
-}
-
-async function makedistinct(input) {
+// Only considers subject of verb, does not consider otherSubject.
+async function makedistinct(input, bClearClear = false) {
   var out = [];
   var already = new Set();
   for (var j of input) {
@@ -257,12 +371,10 @@ async function makedistinct(input) {
     //   delegate, can't just clear.
     // Con:
     // - Performance.
-    if (verb == 'clear') continue;
-    delete j.I;
-    delete j.statement;
+    if (bClearClear) {
+      if (verb == 'clear') continue;
+    }
     // TODO: Teach Dart Jsonish to accept our token so that we can delete [signature, previous]
-    // delete j.signature;
-    // delete j.previous;
     out.push(j);
   }
   return out;
@@ -273,52 +385,11 @@ async function makedistinct(input) {
 exports.clouddistinct = onCall(async (request) => {
   // const token = req.query.token;
   const token = request.data.token;
-  if (!token) return res.status(400).send('Missing token');
-
+  logger.log(request.data);
   try {
-    const db = admin.firestore();
-    const collectionRef = db.collection(token).doc('statements').collection('statements');
-    const snapshot = await collectionRef.orderBy('time', 'desc').get();
-    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-
-    // Do this before distinct call below as the "delete" operations there affect the underlying objects.
-    var iKey;
-    var lastToken;
-    if (data.length > 0) {
-      iKey = data[0].I;
-      lastToken = data[data.length - 1].id;
-    }
-
-    // Validate notary chain, decending order
-    var first = true;
-    var previousToken;
-    var previousTime;
-    for (var d of data) {
-      if (first) {
-        first = false; // no check
-      } else {
-        if (d.id != previousToken) {
-          var error = `Notarization violation: ${d.id} != ${previousToken}`;
-          logger.error(error);
-          // TEMP: throw error;
-        }
-
-        if (d.time >= previousTime) {
-          var error = `Not descending: ${d.time} >= ${previousTime}`;
-          logger.error(error);
-          // TEMP: throw error;
-        }
-      }
-      previousToken = d.previous;
-      previousTime = d.time;
-    }
-
-    var distinct = await makedistinct(data);
-
-    return { "iKey": iKey, "lastToken": lastToken, "statements": distinct };
+    return await fetchh(token, request.data, request.data.omit);
   } catch (error) {
     console.error(error);
-    // res.status(500).send('Error exporting collection');
     throw new HttpsError(error);
   }
 });
